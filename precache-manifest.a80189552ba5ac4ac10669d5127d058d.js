0528553e975e7ef3a26cc390d6466258c53b90bc
Jonathan William Silva dos Santos <jonathanwilliam.santos@southsystem.com.br>
2021-05-09 16:01:07 -0300

    Updates

---
+++ precache-manifest.a80189552ba5ac4ac10669d5127d058d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da794088ae6ec48307ed50851b8bad45",
+    "url": "/frontend-react/index.html"
+  },
+  {
+    "revision": "faf9556e5375ffa9e1b7",
+    "url": "/frontend-react/static/css/main.2647837b.chunk.css"
+  },
+  {
+    "revision": "f9031a443096bf2650a9",
+    "url": "/frontend-react/static/js/2.254e593d.chunk.js"
+  },
+  {
+    "revision": "faf9556e5375ffa9e1b7",
+    "url": "/frontend-react/static/js/main.1ef4b10f.chunk.js"
+  },
+  {
+    "revision": "eb9558112551b88c8fbd",
+    "url": "/frontend-react/static/js/runtime~main.56c0abad.js"
+  },
+  {
+    "revision": "a8cd944603c5fbbee81fd995d2aa851e",
+    "url": "/frontend-react/static/media/logo.a8cd9446.svg"
+  }
+]);
